66777a129d653d491d13ec3b49ebd1e5142b5ee3
trhgquan <quanthcstpbt@gmail.com>
2021-11-19 23:25:43 +0700

    Finishing sentiment_analysis

---
+++ sentiment_analysis/readme.txt
@@ -0,0 +1,15 @@
+Sinh viên thực hiện: Trần Hoàng Quân
+MSSV: 19120338
+
+Hướng dẫn sử dụng:
+
+Bước 1:
+- Cài đặt các thư viện cần thiết (pip install -r requirements.txt)
+
+Train model:
+= Mở danh_gia_mon_hoc_bayes.ipynb và chạy tuần tự từ trên xuống dưới để
+generate model và vectorizer.
+
+Chạy chương trình:
+- Có thể sử dụng trực tiếp trong notebook như demo
+- Chạy sentiment_analysis.py --input="Câu cần phân tích" --result=<File output>
---
+++ sentiment_analysis/sentiment_analysis.py
@@ -0,0 +1,55 @@
+import joblib, getopt, sys
+from underthesea import word_tokenize
+
+# Tiền xử lý: loại bỏ các dấu , . ! ?,
+# loại bỏ dấu cách thừa và chuyển string về dạng lowercase.
+def preprocess(sentence):
+    sentence = sentence.strip()
+    sentence = sentence.replace(',', '')
+    sentence = sentence.replace('.', '')
+    sentence = sentence.replace('?', '')
+    sentence = sentence.replace('!', '')
+    sentence = sentence.replace('"', '')
+    sentence = sentence.replace('  ', ' ')
+    return sentence.lower().strip()
+
+def tokenize(sentence):
+    return word_tokenize(sentence, format = 'word')
+
+def main(argv):
+    input, output_file = '', ''
+    try:
+        opts, args = getopt.getopt(argv, 'hi:o:', ['input=', 'result='])
+    except getopt.GetoptError:
+        print('Command line arguments invalid, try again.')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '--input':
+            input = arg
+        elif opt == '--result':
+            output_file = arg
+
+    if (input == '' or output_file == ''):
+        print('Missing arguments, please try again.')
+        sys.exit(2)
+
+    stopwords = []
+    with open('stopwords.txt', 'r+', encoding = 'utf-8') as f:
+        for line in f:
+            stopwords.append(line.strip())
+
+    try:
+        model_ = joblib.load('sentiment.joblib')
+        vectorizer_ = joblib.load('vectorizer.joblib')
+
+        with open(output_file, 'w+') as f:
+            predict_ = model_.predict(vectorizer_.transform([preprocess(input)]))
+            print(predict_[0], file = f)
+
+        print('Successfully printed to {0}'.format(output_file))
+    except:
+        print('Model not found, have you tried running training inside danh_gia_mon_hoc_bayes.ipynb yet?')
+
+if __name__ == '__main__':
+    main(sys.argv[1:]);
